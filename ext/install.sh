b87c4c1ff1d3d05618903db086997fc816db5019
Lijun Liao <lijun.liao@gmail.com>
2016-05-05 19:24:48 +0200

    simplified the process to install maven dependency sunpkcs11-wrapper

--- ext/install.sh
+++ ext/install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 # pkcs11-wrapper
-mvn install:install-file -Dfile=pkcs11-wrapper-1.3.jar -DgroupId=at.tugraz.iaik \
-  -DartifactId=pkcs11-wrapper -Dversion=1.3 -Dpackaging=jar
-
-mvn install:install-file -Dfile=pkcs11-wrapper-native-1.3.zip -DgroupId=at.tugraz.iaik \
-  -DartifactId=pkcs11-wrapper-native -Dversion=1.3 -Dpackaging=zip
+mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.0.jar -DgroupId=org.xipki.iaik \
+  -DartifactId=sunpkcs11-wrapper -Dversion=1.3.0 -Dpackaging=jar
 
