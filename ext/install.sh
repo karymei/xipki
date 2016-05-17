c00739d7e5d2a61889cf6a99085f236b207b17a5
Lijun Liao <lijun.liao@gmail.com>
2016-05-17 23:28:06 +0200

    now the jscep.script works

--- ext/install.sh
+++ ext/install.sh
@@ -4,3 +4,6 @@
 mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.0.jar -DgroupId=org.xipki.iaik \
   -DartifactId=sunpkcs11-wrapper -Dversion=1.3.0 -Dpackaging=jar
 
+# jscep (the current release 2.3.0 contains bug that sends incorrect issuer DN
+mvn install:install-file -Dfile=jscep-2.4.0-SNAPSHOT.jar -DgroupId=com.google.code.jscep \
+  -DartifactId=jscep -Dversion=2.4.0-SNAPSHOT -Dpackaging=jar
