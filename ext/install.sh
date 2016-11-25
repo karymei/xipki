9109cecc363bd63cc4d4d8757100af2984c96bc7
Lijun Liao <lijun.liao@gmail.com>
2016-11-25 20:20:54 +0100

    use the version 1.56-b06 of BouncyCastle libraries

--- ext/install.sh
+++ ext/install.sh
@@ -5,7 +5,7 @@ mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.0.jar -DgroupId=org.xipki.
   -DartifactId=sunpkcs11-wrapper -Dversion=1.3.0 -Dpackaging=jar
 
 # bouncycastle (version 1.55 cannot parse OCSP CertStatus with unknown status)
-mvn install:install-file -Dfile=bc/bcprov-jdk15on-156b03.jar -DpomFile=bc/bcprov-jdk15on.pom
+mvn install:install-file -Dfile=bc/bcprov-jdk15on-156b06.jar -DpomFile=bc/bcprov-jdk15on.pom
 
-mvn install:install-file -Dfile=bc/bcpkix-jdk15on-156b03.jar -DpomFile=bc/bcpkix-jdk15on.pom
+mvn install:install-file -Dfile=bc/bcpkix-jdk15on-156b06.jar -DpomFile=bc/bcpkix-jdk15on.pom
 
