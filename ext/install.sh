307638ab17abb734e13672bf9a6551018dfb3473
Lijun Liao <lijun.liao@gmail.com>
2017-04-08 07:43:43 +0200

    added the source jar of sunpkcs11-wrapper

--- ext/install.sh
+++ ext/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # pkcs11-wrapper
-mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.0.jar -DgroupId=org.xipki.iaik \
-  -DartifactId=sunpkcs11-wrapper -Dversion=1.3.0 -Dpackaging=jar
+mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.0.jar \
+  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.3.0 \
+  -Dpackaging=jar
 
+mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.0-sources.jar \
+  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.3.0 \
+  -Dpackaging=jar -Dclassifier=sources
