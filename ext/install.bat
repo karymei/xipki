4a22f28cd75a39b7b49f27d1a0e8ea13d13e5bae
Lijun Liao <lijun.liao@gmail.com>
2017-06-04 04:33:54 +0200

    updated sunpkcs11-wrapper to 1.4.0

--- ext/install.bat
+++ ext/install.bat
@@ -1,8 +1,8 @@
 rem  pkcs11-wrapper
-call mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.1.jar ^
-  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.3.1 ^
+call mvn install:install-file -Dfile=sunpkcs11-wrapper-1.4.0.jar ^
+  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.4.0 ^
   -Dpackaging=jar
 
-call mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.1-sources.jar ^
-  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.3.1 ^
+call mvn install:install-file -Dfile=sunpkcs11-wrapper-1.4.0-sources.jar ^
+  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.4.0 ^
   -Dpackaging=jar -Dclassifier=sources
--- ext/install.sh
+++ ext/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # pkcs11-wrapper
-mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.1.jar \
-  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.3.1 \
+mvn install:install-file -Dfile=sunpkcs11-wrapper-1.4.0.jar \
+  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.4.0 \
   -Dpackaging=jar
 
-mvn install:install-file -Dfile=sunpkcs11-wrapper-1.3.1-sources.jar \
-  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.3.1 \
+mvn install:install-file -Dfile=sunpkcs11-wrapper-1.4.0-sources.jar \
+  -DgroupId=org.xipki.iaik -DartifactId=sunpkcs11-wrapper -Dversion=1.4.0 \
   -Dpackaging=jar -Dclassifier=sources
