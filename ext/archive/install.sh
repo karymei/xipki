b203a6973f7f995cd6e6aae8ec7b42c4e995c7ad
Lijun Liao <lijun.liao@gmail.com>
2017-11-28 07:05:42 +0100

    embed the submodules to the main build process

--- ext/archive/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# pkcs11-wrapper
-mvn install:install-file -Dfile=pkcs11-wrapper-1.3.jar -DgroupId=at.tugraz.iaik \
-  -DartifactId=pkcs11-wrapper -Dversion=1.3 -Dpackaging=jar
-
-mvn install:install-file -Dfile=pkcs11-wrapper-native-1.3.zip -DgroupId=at.tugraz.iaik \
-  -DartifactId=pkcs11-wrapper-native -Dversion=1.3 -Dpackaging=zip
-
